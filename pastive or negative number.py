7e19da461d00654b1902961a73b76c8b13c12564
MATHIYAZHAGAN R <rmathiyazhagan46@gmail.com>
2026-07-14 15:55:08 +0530

    Add files via upload
    these are basic pyhon problems

---
+++ pastive or negative number.py
@@ -0,0 +1,10 @@
+num=int(input("enter the mathi :"))
+if num>0:
+    print('this is postive')
+elif num<0:
+    print('this is nagative')
+else:
+    print('this is invalide')
+          
+          
+    
